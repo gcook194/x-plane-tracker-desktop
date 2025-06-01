c50d8ee5039c9a909790cbbc33bf3f869f20d36c
Gavin Cook <gavincook1994@hotmail.co.uk>
2025-06-01 01:15:17 +0100

    first pass at making deployable

--- src/main/java/com/gav/xplanetracker/database/DatabaseConnection.java
+++ src/main/java/com/gav/xplanetracker/database/DatabaseConnection.java
@@ -3,6 +3,9 @@ package com.gav.xplanetracker.database;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -15,8 +18,13 @@ public class DatabaseConnection {
 
     public static Connection connect() {
         try {
-            return DriverManager.getConnection(URL);
+            String userDir = System.getProperty("user.dir");
+            String dbPath = userDir + File.separator + "db" + File.separator + "sqlite" + File.separator + "flights.db";
+            System.out.println(dbPath);
+
+            return DriverManager.getConnection("jdbc:sqlite:" + dbPath);
         } catch (SQLException e) {
+            e.printStackTrace();
             logger.error("SQLite connection failed: " + e.getMessage(), e);
             return null;
         }
